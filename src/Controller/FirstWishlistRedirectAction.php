1007b8c3490e7da508248f27cb46c9b007d38d3e
Joachim Løvgaard <joachim@loevgaard.dk>
2024-12-03 08:46:16 +0100

    Fixed the 'first wishlist redirect action' to only redirect if a wishlist exists

--- src/Controller/FirstWishlistRedirectAction.php
+++ src/Controller/FirstWishlistRedirectAction.php
@@ -4,42 +4,50 @@ declare(strict_types=1);
 
 namespace Setono\SyliusWishlistPlugin\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
-use Setono\Doctrine\ORMTrait;
-use Setono\SyliusWishlistPlugin\Factory\WishlistFactoryInterface;
 use Setono\SyliusWishlistPlugin\Provider\WishlistProviderInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\FlashBagAwareSessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * This class is used to redirect the user to the first wishlist.
  * This is useful if your application only needs one wishlist per user.
- *
- * NOTICE that it will persist the wishlist if it doesn't exist.
  */
 final class FirstWishlistRedirectAction
 {
-    use ORMTrait;
 
     public function __construct(
         private readonly WishlistProviderInterface $wishlistProvider,
-        private readonly WishlistFactoryInterface $wishlistFactory,
-        ManagerRegistry $managerRegistry,
         private readonly UrlGeneratorInterface $urlGenerator,
     ) {
-        $this->managerRegistry = $managerRegistry;
     }
 
-    public function __invoke(): RedirectResponse
+    public function __invoke(Request $request): RedirectResponse
     {
-        $wishlist = $this->wishlistProvider->getWishlists()[0];
+        $wishlists = $this->wishlistProvider->getWishlists();
+        if ([] === $wishlists) {
+            $session = $request->getSession();
+            if ($session instanceof FlashBagAwareSessionInterface) {
+                $session->getFlashBag()->add('info', 'setono_sylius_wishlist.no_wishlists');
+            }
 
-        $this->getManager($wishlist)->persist($wishlist);
-        $this->getManager($wishlist)->flush();
+            return $this->redirect($request);
+        }
 
         return new RedirectResponse($this->urlGenerator->generate(
             'setono_sylius_wishlist_shop_wishlist_show',
-            ['uuid' => $wishlist->getUuid()],
+            ['uuid' => $wishlists[0]->getUuid()],
         ));
+    }
+
+    private function redirect(Request $request): RedirectResponse
+    {
+        $referrer = $request->headers->get('referer');
+        if (is_string($referrer) && '' !== $referrer) {
+            return new RedirectResponse($referrer);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('sylius_shop_homepage'));
     }
 }
